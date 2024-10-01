139aa9b8122df96ced05669cd8f641c7bc209867
han <hanbong5938@gmail.com>
2024-10-01 23:24:39 +0900

    feat: 지도 라이브러리 추가

--- src/pages/Weather.tsx
+++ src/pages/Weather.tsx
@@ -1,25 +1,47 @@
-import map from "../assets/map.jpg"
 import {ForecastTable} from "../components";
 import {useTranslation} from "react-i18next";
 import {ForecastRequest} from '../dto';
 import {format, subDays} from 'date-fns';
-
+import React, {useEffect, useState} from "react";
+import Southkorea from "@react-map/south-korea";
 
 const Weather = () => {
     const sevenDaysAgo = subDays(new Date(), 7);
     const formattedDate = format(sevenDaysAgo, 'yyyy-MM-dd');
 
-    const initialRequest = new ForecastRequest('Seoul', formattedDate);
     const {t} = useTranslation();
+    const [selectedRegion, setSelectedRegion] = useState('Seoul');
+    const [initialRequest, setInitialRequest] = useState(new ForecastRequest(selectedRegion, formattedDate));
+
+    useEffect(() => {
+        setInitialRequest(new ForecastRequest(selectedRegion, formattedDate));
+    }, [selectedRegion, formattedDate]);
+
+    const handleRegionSelect = (state: string) => {
+        setSelectedRegion(state);
+        alert(state)
+    };
+
     return (
         <section className="max-w-7xl mx-auto border-b-2">
             <div className="flex flex-col items-center my-20">
-                <img src={map} className="w-1/2 h-auto object-cover rounded-2xl p-2" alt={'map'}/>
+                <Southkorea
+                    type="select-single"
+                    onSelect={handleRegionSelect}
+                    selectColor="skyblue"
+                    mapColor="#E2E8F0"
+                    strokeColor="#A0AEC0"
+                    hoverColor="#A0AEC0"
+                    size={600}
+                />
             </div>
-            <h1 className="text-center text-2xl font-bold my-4">{t('page.weather.yearly.weather.data')}</h1>
+
+            <h1 className="text-center text-2xl font-bold my-4">
+                {t('page.weather.yearly.weather.data')}
+            </h1>
             <ForecastTable initialRequest={initialRequest}/>
         </section>
-    )
-}
+    );
+};
 
-export default Weather
+export default Weather;
